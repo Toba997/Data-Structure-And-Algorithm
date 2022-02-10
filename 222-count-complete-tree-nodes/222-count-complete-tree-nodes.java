846cca32eaf9c63cc96942cd673d5fbda8e428e8
Toba997 <88072424+Toba997@users.noreply.github.com>
2022-02-10 14:21:13 +0530

    Time: 0 ms (100.00%), Space: 50.7 MB (5.33%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -16,6 +16,27 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
-        return 1+countNodes(root.left)+countNodes(root.right);
+        int left = leftHeight(root);
+        int right = rightHeight(root);
+        
+        if(left == right) return ((2<<(left)) -1);
+        else return countNodes(root.left)+countNodes(root.right)+1;
+        
+    }
+    public int leftHeight(TreeNode root){
+        int count = 0;
+        while(root.left!=null){
+            count++;
+            root=root.left;
+        }
+        return count;
+    }
+    public int rightHeight(TreeNode root){
+        int count = 0;
+        while(root.right!=null){
+            count++;
+            root=root.right;
+        }
+        return count;
     }
 }
