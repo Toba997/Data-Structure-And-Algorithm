3abbc3a8ae0a85d2ae59d1d825a84c336f9a60e6
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-04-08 10:44:16 +0530

    Time: 24 ms (86.21%), Space: 59.4 MB (41.22%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       List<List<Integer>> res = new ArrayList<>();
-    Arrays.sort(nums);
-    for (int i = 0; i + 2 < nums.length; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
-            continue;
-        }
-        int j = i + 1, k = nums.length - 1;  
-        int target = -nums[i];
-        while (j < k) {
-            if (nums[j] + nums[k] == target) {
-                res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                j++;
-                k--;
-                while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
-                while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
-            } else if (nums[j] + nums[k] > target) {
-                k--;
-            } else {
-                j++;
+   Arrays.sort(nums);
+        
+        List<List<Integer>> res = new LinkedList<>(); 
+        
+        for (int i = 0; i < nums.length-2; i++) {
+            
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                
+                int lo = i+1, hi = nums.length-1, sum = 0 - nums[i];
+                
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        
+                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        
+                        lo++; hi--;
+                    } 
+                    else if (nums[lo] + nums[hi] < sum) lo++;
+                    
+                    else hi--;
+               }
             }
         }
+        return res;
     }
-    return res;
-}
 }
