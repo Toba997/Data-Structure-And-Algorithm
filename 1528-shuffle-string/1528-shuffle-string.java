dac665d89c033c9eac345ae845aa61f2990a62c3
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-04-10 13:04:56 +0530

    Time: 15 ms (7.38%), Space: 46.7 MB (11.65%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -4,6 +4,10 @@ class Solution {
         for(int i=0;i<s.length();i++){
             arr[indices[i]]=s.charAt(i);
         }
-        return new String(arr);
+        String res ="";
+        for(char element : arr){
+            res=res+element;
+        }
+        return res;
     }
 }
