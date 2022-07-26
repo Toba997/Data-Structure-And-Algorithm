1935a9bc244f3a5a1762ce43f2327f7271563b46
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-07-26 15:22:56 +0530

    Time: 71 ms (32.21%), Space: 106.8 MB (47.00%) - LeetHub

---
+++ 575-distribute-candies/575-distribute-candies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        int n = candyType.length/2;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0;i<candyType.length;i++){
+            set.add(candyType[i]);
+        }
+        
+        if(n>=set.size()){
+            return set.size();
+        }
+        return n;
+    }
+}
