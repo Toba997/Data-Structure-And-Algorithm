de5048a1b8d886b1d73b9faa4b158c474ac2c0d5
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-08-01 17:57:32 +0530

    Time: 5 ms (64.51%), Space: 45.2 MB (56.22%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,14 +1,65 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
+        int n=nums.length;
+       
+        int start = 0;
+        int end = nums.length-1;
+        
+        int n1 = 0;
+        int n2 = 0;
+        
+        int temp[] = new int[nums.length];
         int res [] = new int[2];
+        
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
+            temp[i]=nums[i];
+        }
+        Arrays.sort(nums);
+        
+        while(start<end){
+            int sum = nums[start]+nums[end];
+            
+            if(sum==target){
+                n1 = nums[start];
+                n2 = nums[end];
+                break;
+            }
+            
+            if(sum<target){
+                start++;
+            }else{
+                end--;
+            }
+        }
+        if(nums[start]!=nums[end]){
+            for(int i=0;i<temp.length;i++){
+                if(n1==temp[i]){
                     res[0] = i;
-                    res[1] = j;
+                }
+                if(n2==temp[i]){
+                    res[1] = i;
+                }
+            }
+        }else{
+            int k=0;
+            for(;k<n;k++){
+                if(temp[k]==nums[start]){
+                    res[0]=k;
+                    break;
+                }
+            }
+                k++;
+                for(;k<n;k++){
+                if(temp[k]==nums[end]){
+                    res[1]=k;
+                    break;
                 }
             }
         }
-        return res;
+       return res;
     }
 }
+
+
+// 2 3 5 7 8 9 t = 14
