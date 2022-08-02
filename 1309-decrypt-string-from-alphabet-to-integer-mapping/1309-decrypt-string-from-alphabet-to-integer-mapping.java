ee224e1ef5750411e03f71cc27d76f3814576f8a
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-08-02 14:34:21 +0530

    Time: 15 ms (11.34%), Space: 43.3 MB (14.93%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String freqAlphabets(String s) {
+        
+        String ans = "";
+        
+        for(int i=s.length()-1;i>=0;i--){
+            char c = s.charAt(i);
+            if(s.charAt(i)=='#'){
+                    String updated = s.charAt(i-2)+""+s.charAt(i-1);
+                    
+                    ans = (char)(Integer.parseInt(updated)+96) + ans;
+                    
+                    i=i-2;
+            }else {
+                // update my ans from last.
+                // cast the complete expression to char. add my c+96. 
+                ans = (char)(Integer.parseInt(String.valueOf(c))+96) + ans;
+            }
+        }
+
+        // return the finall String ans
+        return ans;
+    }
+}
