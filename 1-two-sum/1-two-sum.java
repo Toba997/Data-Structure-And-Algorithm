397bb09404b5ee109216183ee2438c4e53808df6
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-08-10 10:29:04 +0530

    Time: 6 ms (58.43%), Space: 47.6 MB (5.14%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,65 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        int n=nums.length;
-       
-        int start = 0;
-        int end = nums.length-1;
-        
-        int n1 = 0;
-        int n2 = 0;
-        
-        int temp[] = new int[nums.length];
-        int res [] = new int[2];
-        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int [] ans = new int[2];
         for(int i=0;i<nums.length;i++){
-            temp[i]=nums[i];
-        }
-        Arrays.sort(nums);
-        
-        while(start<end){
-            int sum = nums[start]+nums[end];
-            
-            if(sum==target){
-                n1 = nums[start];
-                n2 = nums[end];
-                break;
-            }
-            
-            if(sum<target){
-                start++;
+            if(map.containsKey(target-nums[i])){
+                ans[1] = i;
+                ans[0] = map.get(target-nums[i]);
             }else{
-                end--;
+                map.put(nums[i],i);
             }
         }
-        if(nums[start]!=nums[end]){
-            for(int i=0;i<temp.length;i++){
-                if(n1==temp[i]){
-                    res[0] = i;
-                }
-                if(n2==temp[i]){
-                    res[1] = i;
-                }
-            }
-        }else{
-            int k=0;
-            for(;k<n;k++){
-                if(temp[k]==nums[start]){
-                    res[0]=k;
-                    break;
-                }
-            }
-                k++;
-                for(;k<n;k++){
-                if(temp[k]==nums[end]){
-                    res[1]=k;
-                    break;
-                }
-            }
-        }
-       return res;
+        return ans;
     }
 }
-
-
-// 2 3 5 7 8 9 t = 14
