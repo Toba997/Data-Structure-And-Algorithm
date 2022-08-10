d29bf18a27a93ebea097ca9ec32105a1a9dccde5
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-08-10 12:40:39 +0530

    Time: 34 ms (58.69%), Space: 59.2 MB (45.91%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,33 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-   Arrays.sort(nums);
         
-        List<List<Integer>> res = new ArrayList<>(); 
+        Arrays.sort(nums);
         
-        for (int i = 0; i < nums.length-2; i++) {
-            
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                
-                int lo = i+1, hi = nums.length-1, sum = 0 - nums[i];
-                
-                while (lo < hi) {
-                    if (nums[lo] + nums[hi] == sum) {
-                        
-                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                        
-                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
-                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
-                        
-                        lo++; hi--;
-                    } 
-                    else if (nums[lo] + nums[hi] < sum) lo++;
-                    
-                    else hi--;
-               }
+        List<List<Integer>> list = new LinkedList<>();
+        
+        
+        for(int i=0;i<nums.length-2;i++){
+            if(i==0 || i>0 && nums[i]!=nums[i-1]){
+                int low = i+1, high = nums.length-1;
+                int sum = 0-nums[i];
+
+                while(low<high){
+                    if(nums[low]+nums[high]==sum){
+                        list.add(Arrays.asList(nums[low],nums[high],nums[i]));
+
+                        while(low<high && nums[low]==nums[low+1]) low++;
+                        while(low<high && nums[high]==nums[high-1]) high--;
+
+                        low++;
+                        high--;
+
+                    }else if(nums[low]+nums[high]<sum){
+                        low++;
+                    }else{
+                        high--;
+                    }
+                }
             }
         }
-        return res;
+        return list;
     }
 }
-
-
