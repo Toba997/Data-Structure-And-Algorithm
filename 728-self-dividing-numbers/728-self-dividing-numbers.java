97d20856b8294a8232303b916a1d3bcd58c955b3
Sourav Verma <88072424+Toba997@users.noreply.github.com>
2022-08-17 10:46:27 +0530

    Time: 2 ms (99.91%), Space: 42 MB (42.32%) - LeetHub

--- 728-self-dividing-numbers/728-self-dividing-numbers.java
+++ 728-self-dividing-numbers/728-self-dividing-numbers.java
@@ -1,21 +1,21 @@
 class Solution {
-    public static boolean Check(int x){
-        
+
+    public static boolean Check(int x) {
         int temp = x;
-        while(x>0){
+        while (x > 0) {
             int rem = x % 10;
-            if(rem == 0 || temp%rem!=0){
+            if (rem==0 || temp % rem != 0) {
                 return false;
             }
-            x=x/10;
+            x = x / 10;
         }
         return true;
     }
+
     public List<Integer> selfDividingNumbers(int left, int right) {
-        
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i=left;i<=right;i++){
-            if(Check(i)){
+        for (int i = left; i <= right; i++) {
+            if (Check(i)) {
                 list.add(i);
             }
         }
